5bfe7c61f2f8b7810bffff253955f0c0fa1dd49f
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2026-04-08 06:19:41 -0700

    add ui dependencies

---
+++ app/(admin)/admin/users/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { motion } from "motion/react";
+import { Search, Shield, ShieldBan, MoreVertical, ActivitySquare } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function UserManagementPage() {
+    return (
+        <div></div>
+    );
+}
