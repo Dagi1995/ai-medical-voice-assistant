8d283cc77baa89cdf0e3c0d6c581e12ae472831b
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2026-04-08 07:07:16 -0700

    intialize and add dependencies

---
+++ app/(admin)/admin/appointments/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { motion } from "motion/react";
+import { Search, CalendarDays, CheckCircle2, XCircle, Clock } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function AppointmentsPage() {
+    return (
+        <div></div>
+    );
+}
